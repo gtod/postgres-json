10f37b0901b2af38a537811dc4fc7046ba95f290
Gregory Tod <vlad@gtod.net>
2014-11-23 16:42:02 +1100

    Clean up tests file
    Compiles and runs (given a backend)

--- tests.lisp
+++ tests.lisp
@@ -1,31 +1,43 @@
 (defpackage :pj-test
   (:nicknames :pj-test)
-  (:use :cl :postgres-json)
-  (:import-from :postmodern :connect-toplevel :with-connection))
+  (:use :cl :postmodern :postgres-json)
+  (:import-from :postgres-json :obj :pp-json))
 
 (in-package :pj-test)
 
-(defun hash-list (&rest args)
-  "Return an 'equal hash-table consisting of pairs of ARGS."
-  (let ((hash (make-hash-table :test #'equal)))
-    (loop for (key val) on args by #'cddr do
-      (setf (gethash key hash) val))
-    hash))
-
-;;; In PSQL: set search_path pgj_schema,public;
+;;; See quickstart to ensure you have created the default schema,
+;;; sequence and the backend for cat
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (bake-interface cat))
 
-(defun insert-some (number)
+(defun insert-some (&optional (number 40))
   (dotimes (i number)
-    (cat:insert (hash-list "name" (format nil "name-~A" i) "coat" "scruffy"))))
+    (cat:insert (obj "name" (format nil "name-~A" i) "coat" "scruffy"))))
+
+;; quickload bordeaux-threads if need be
+
+(log:config :debug)
 
 (defun update-some ()
   (flet ((update-cat (id)
            (bt:make-thread
             (lambda ()
               (with-connection '("cusoon" "gtod" "" "localhost" :port 5433)
-                (cat:update id (hash-list "name" (format nil "name-~A" id) "coat" "scruffy")))))))
+                (cat:update id (obj "name" (format nil "name-~A" id) "coat" "scruffy")))))))
     (loop for id from 4 to 23
           do (update-cat id))))
+
+;; In PSQL: set search_path pgj_schema,public;
+;; now try select count(*) from cat_old;
+
+(defun update-one ()
+  (flet ((update-cat (id)
+           (bt:make-thread
+            (lambda ()
+              (with-connection '("cusoon" "gtod" "" "localhost" :port 5433)
+                (cat:update id (obj "name" (format nil "name-~A" id) "coat" "scruffy")))))))
+    (dotimes (i 20)
+      (update-cat 1))))
+
+;; now try select count(*) from cat_old;
