8874f1bb1203cffa5526138d7cb59fdbcf475ff1
Gregory Tod <vlad@gtod.net>
2014-11-27 22:26:06 +1100

    tests now compile and run again

--- tests.lisp
+++ tests.lisp
@@ -1,3 +1,6 @@
+;;;; Some informal tests for the REPL
+;;;; Requires a previous call to CREATE-BACKEND
+
 (defpackage :pj-test
   (:use :cl :postgres-json)
   (:shadowing-import-from :postgres-json :get :delete)
@@ -5,38 +8,24 @@
 
 (in-package :pj-test)
 
-(defvar *test-schema* 'net-gtod-postgres-json-test-schema)
-
-(defparameter *test-schema-exists-message*
-  "Not willing to run tests in existing schema.  Unless this schema
-contains important data of yours (unlikey given the name its got but
-you can never be too careful with other people's data), you might want
-to drop it with (drop-db-schema-cascade *test-schema*) and try
-again.~%Schema: ~A")
+(defparameter *booking* 'gtod-net-booking-test)
+(defparameter *cat* 'gtod-net-cat-test)
 
 (defparameter *bookings-json*
   (merge-pathnames "tests.json" (asdf:system-source-directory :postgres-json)))
 
 (defun setup ()
-  (if (pomo:schema-exist-p *test-schema*)
-      (error *test-schema-exists-message* *test-schema*)
-      (let ((*db-schema* *test-schema*))
-        (create-db-schema)
-        (create-db-sequence)
-        (create-model-backend 'booking)
-        (bake-model 'booking)
-
-        (create-model-backend 'cat)
-        (bake-model 'cat))))
+  (create-model *booking*)
+  (create-model *cat*))
 
 (defun insert-bookings ()
   (alexandria:with-input-from-file (stream *bookings-json*)
     (dolist (booking (yason:parse stream))
-      (insert 'booking booking))))
+      (insert *booking* booking))))
 
 (defun insert-some-cats (&optional (number 40))
   (dotimes (i number)
-    (insert 'cat (obj "name" (format nil "name-~A" i) "coat" "scruffy"))))
+    (insert *cat* (obj "name" (format nil "name-~A" i) "coat" "scruffy"))))
 
 ;; ;; quickload bordeaux-threads for tests below...
 
