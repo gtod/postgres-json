18e042e17675b0c316bec25e2e90d74406708eca
Gregory Tod <vlad@gtod.net>
2014-11-27 22:19:16 +1100

    Clean up model creation with model parameters
    See how we bind the specials to the params?  Those *same* params will be
    bound we the make query functions are called on demand.  That is how it
    should be (of course) and was a big mess when everything was just a
    special variable (public or otherwise).
    
    The neat trick is the INSERT call at the botton of create-model.  We are
    eating our own dog food by keeping model parameters in our own model
    table!

--- interface.lisp
+++ interface.lisp
@@ -51,29 +51,43 @@ in a given PostgreSQL database."
         (create-model *meta-model* (meta-model-parameters))
         *pgj-schema*)))
 
-;; Investigate calling deallocate to drop prepared queries
-;; But they are probably connection specific anyway...
-;; What are the consequences of that?
-(defun bake-model (model &key (schema *db-schema*) (sequence *db-sequence*))
-  "Prepare all the PostgreSQL queries necessary to support PostgreSQL
-JSON persistence model calls on MODEL, a symbol.  SCHEMA and SEQUENCE,
-both symbols, may be specified to use a specific DB schema and/or
-sequence for primary keys, respectively."
-  (let* ((base model)
-         (old (sym t base "-old"))
-         (table (qualified-name base schema))
-         (table-old (qualified-name old schema)))
-    (make-nextval-sequence$ model (qualified-name-string sequence schema))
-    (make-insert$ model table)
-    (make-insert-old$ model table table-old)
-    (make-update$ model table)
-    (make-get$ model table)
-    (make-delete$ model table)
-    (make-keys$ model table)
-    model))
+(defun create-model (model &optional (parameters (make-model-parameters)))
+  "Create the PostgreSQL tables and indexes for PostgreSQL JSON
+persistence model MODEL, a symbol.  This should only be called once
+per model.  Returns MODEL."
+  (flet ((param (key)
+           (gethash key parameters)))
+    ;; We could, of course, pass the parameters hash around instead...
+    (let ((*id* (param "id"))
+          (*id-type* (param "id-type"))
+          (*jdoc* (param "jdoc"))
+          (*jdoc-type* (param "jdoc-type")))
+      (let* ((schema *pgj-schema*)
+             (name model)
+             (name-old (sym t name "-old"))
+             (index (sym t name "-gin"))
+             (index-old (sym t name "-old-gin")))
+        (create-base-table name schema)
+        (create-old-table name-old schema)
+        (when (eq 'jsonb *jdoc-type*)
+          (create-gin-index index name schema)
+          (create-gin-index index-old name-old schema))
+        (unless (eq *meta-model* model)
+          (insert *meta-model* (maphash-symbols-to-strings parameters)
+                  :use-id (symbol-name model)))
+        model))))
 
-;; (defun drop-backend (name))
-;; (defun delete-model (name))
+(defun make-model-parameters (&key (sequence *pgj-sequence*) (id *id*) (id-type *id-type*)
+                                   (jdoc *jdoc*) (jdoc-type *jdoc-type*))
+  "Create a hash-table of parameters to specify backend features of a
+PostgreSQL JSON persistence model, typically to be supplied to
+CREATE-MODEL.  For each keyword argument see the documentation of the
+special variable that is its default value."
+  (obj "sequence" sequence
+       "id" id
+       "id-type" id-type
+       "jdoc" jdoc
+       "jdoc-type" jdoc-type))
 
 ;;;; SEMI PUBLIC
 
