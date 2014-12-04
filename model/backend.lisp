1334a7fcdfa7491648877345a133e0dc8a245a30
Gregory Tod <vlad@gtod.net>
2014-12-04 13:44:45 +1100

    Simpler GIN indexing
    This is a premature op (what isn't?) but the comments tell a good story
    and it's pretty easy to revert...

--- model/backend.lisp
+++ model/backend.lisp
@@ -25,15 +25,28 @@ rows.  Requires an active DB connection."
             (,jdoc      :type ,jdoc-type))
            (:primary-key ,key valid-to)))))
 
+;;; Indexing the model table
+
+;; Make jsonb_path_ops the GIN default until needs force otherwise
+;; or we make it an option...
+
+;; In fact we can create a simple BTREE index too:
+;; CREATE INDEX geodata_index ON
+;;    geodata_json ((data->>'country_code'), (data->>'asciiname'));
+;; which will be smaller but you need to explicitly list the keys to index and
+;; it does not support @>
+
+;; It's easy to test and change indexes at run time:
+;; explain analyze select jdoc from booking where jdoc @> '{"state": "pending"}';
+;; drop index booking_gin;
+;; create index booking_gin on booking using GIN (jdoc jsonb_path_ops);
+;; etc...
 (defun create-gin-index (name table model-parameters)
   "Create a PostgreSQL GIN index with NAME on a database table TABLE
 in *PGJ-SCHEMA*, all symbols.  Configure table columns names, types
 etc. using MODEL-PARAMETERS.  Note that if you use 'jdoc-type of 'json
 when creating the tables you cannot then create these indexes --- see
-the PostgreSQL documentation.  In fact the option 'jsonb_path_ops' to
-the GIN index may be desirable under some circumstances but I have not
-yet twisted s-sql to generate such an index declaration (clearly we
-could just use a string).  Requires an active DB connection."
+the PostgreSQL documentation.  Requires an active DB connection."
   (with-readers (jdoc) model-parameters
     (run `(:create-index ,name :on ,(db-name-string table)
-           :using gin :fields ,jdoc))))
+           :using gin :fields (:raw ,(format nil "~A jsonb_path_ops" jdoc))))))
