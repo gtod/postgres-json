92088935796479d3add0c5496edcfb81ca13cf36
Gregory Tod <vlad@gtod.net>
2014-12-07 21:08:01 +1100

    Improve human example

--- examples/human.lisp
+++ examples/human.lisp
@@ -9,13 +9,13 @@
 
 ;;;; Instructions
 
-;;; Compile this file
-;;; Start with these three if you haven't already:
-;;; (pomo:connect-toplevel "<db>" "<user>" "" "localhost")
-;;; (pomo:set-search-path *default-search-path*)
-;;; (create-backend)
+;;; 1. Set this for your Postgres 9.4 DB
+(defparameter *connection* '("cusoon" "gtod" "" "localhost" :port 5433))
 
-;;; Now try (create) then (run-1) and (run-2)
+;;; 2. Compile this file (M-x slime-compile-file in emacs)
+;;; 3. Call (create-backend) if you have not done that elsewhere
+;;; 4. Now review the code of, run, and consider the output of
+;;;    CREATE, LOAD-HUMANS, MODEL-TEST and QUERY-TEST in turn.
 
 ;;; Caveat: the point is to demonstrate the model interface rather
 ;;; than write the most efficient or extensible code.
@@ -29,74 +29,94 @@
 
 ;;; Util
 
+(defmacro with-pj-conn (() &body body)
+  `(pomo:with-connection *connection*
+     ;; There is a small overhead for this, see (alter-role-set-search-path)
+     ;; for a more permanent solution.
+     (pomo:set-search-path *default-search-path*)
+     ,@body))
+
 (defmacro show (form)
   `(progn
      (print ',form)
      (pp-json ,form)))
 
-(defmacro g (key hash)
-  `(gethash ,key ,hash))
-
 (defmacro with-keys ((&rest pairs) object &body body)
   (once-only (object)
     `(symbol-macrolet (,@(loop for pair in pairs
-                               collect `(,(car pair) (g ,(cadr pair) ,object))))
+                               collect `(,(car pair) (gethash ,(cadr pair) ,object))))
        ,@body)))
 
 ;;; Model
 
 (defun random-human ()
-  (let ((keys (pj:keys 'human)))
-    (let ((key (elt keys (random (pj:count 'human)))))
-      (pj:get 'human key))))
+  (pj:get 'human (elt (pj:keys 'human) (random (pj:count 'human)))))
 
 (defun random-gift-type ()
   (elt *gifts* (random (length *gifts*))))
 
 (defun distribute-gifts ()
+  (format t "~%Loading gifts...~%")
   (with-model-transaction ()
     (dotimes (i (round (* (pj:count 'human) 9/10)) (pj:count 'gift))
       (let ((human (random-human)))
-        (let ((gift (obj "human-key" (g "key" human)
+        (let ((gift (obj "human-key" (gethash "key" human)
                          "type" (random-gift-type)
-                         "quantity" (1+ (random 10)))))
+                         "quantity" (1+ (random 30)))))
           (pj:insert 'gift gift))))))
 
 ;;;; Interface
 
 (defun create ()
-  (create-model 'human)
-  (create-model 'gift)
+  (unless (model-exists-p 'human)
+    (with-model-transaction ()
+      (create-model 'human)
+      (create-model 'gift))))
+
+(defun load-humans ()
+  (write-line "Loading humans...")
 
   (unless (probe-file *human-file*)
     (ql-http:fetch *human-url* *human-file*))
 
-  ;; Ahhh, we need empty arrays to go in as emptry arrays!
   (with-input-from-file (stream *human-file*)
     (with-model-transaction ()
+      ;; We need empty arrays to go in as empty arrays, not empty
+      ;; lists (ie. nil) so that :jsonb-array-length can work (see below).
       (loop for human across (yason:parse stream :json-arrays-as-vectors t)
-            do (pj:insert 'human human)))))
+            do (pj:insert 'human human)
+            finally (return (pj:count 'human))))))
+
+(defun cleanup ()
+  (with-model-transaction ()
+    (pj:delete-all 'human)
+    (pj:delete-all 'gift)))
+
+(defun drop ()
+  (drop-model! 'human)
+  (drop-model! 'gift))
 
 ;;; Model functions that lightly wrap the SQL
 
-(defun run-1 ()
-  (show (length (pj:filter 'human :contain (obj "gender" "female"))))
+(defun model-test ()
+  (with-pj-conn ()
+    (show (length (pj:filter 'human :contain (obj "gender" "female"))))
 
-  (let ((keys (pj:keys 'human)))
-    (let ((id (elt keys (random (pj:count 'human)))))
-      (show (g "name" (pj:get 'human id)))))
+    (show (gethash "name" (random-human)))
 
-  (let ((human (show (first (pj:filter 'human :contain (obj "name" "Marcella Marquez"))))))
-    (with-keys ((key "key") (friends "friends")) human
-      (push (obj "name" "Horace Morris") friends)
-      (show (pj:update 'human key human))
-      (show (length (g "friends" (pj:get 'human key))))
-      (show (length (pj:history 'human key)))))
+    (let ((human (show (first (pj:filter 'human :contain (obj "name" "Marcella Marquez"))))))
+      (with-keys ((key "key") (friends "friends")) human
+        (push (obj "name" "Horace Morris" "id" (length friends)) friends)
+        (show (pj:update 'human key human))
+        (show (length (gethash "friends" (pj:get 'human key))))
+        (show (length (pj:history 'human key)))))
 
-  (show (pj:filter 'human :keys '("age" "tags") :contain (obj "tags" '("ut" "labore"))))
-  (show (length (pj:filter 'human :contain (obj "isActive" t "age" 21))))
+    (show (pj:filter 'human :keys '("age" "tags") :contain (obj "tags" '("ut" "labore"))))
+    (show (length (pj:filter 'human :contain (obj "isActive" t "age" 21))))
 
-  (show (length (pj:exists 'human "eyeColor"))))
+    (show (length (pj:exists 'human "eyeColor")))
+    (show (pj:distinct 'human "favoriteFruit")))
+  (values))
 
 ;;;; User defined SQL queries for when FILTER does not cut it.
 
@@ -104,8 +124,8 @@
 ;;; Macroexpand away to see the generated SQL.
 
 ;;; Try slime-edit-definition (M-.) on DEFINE-JSON-QUERY to read
-;;; details on the syntactic sugar of j->, j->> and jbuild and named
-;;; parameter interpolation.
+;;; details on the syntactic sugar of j->, j->>, jbuild, to-json and
+;;; also named parameter interpolation.
 
 (define-json-query rich-humans$ (min-balance gender)
   (:order-by
@@ -114,8 +134,9 @@
    (:select (jbuild ("key" "guid" "gender" "name" "balance"))
 
     :from 'human ;; this is why we need to set the search path above
+                 ;; otherwise it would need to be 'pgj_model.history
 
-    ;; j->> is a little syntactic sugar to get at the top levels keys in 'jdoc
+    ;; j->> is a little syntactic sugar to get at the top level keys in 'jdoc
     ;; j->> returns text, so we must cast to Postgres number type for comparisons
     :where (:and (:>= (:type (j->> "balance") real) min-balance)
                  (:= (j->> "gender") gender))) ;; can also do this using 'containment', see below
@@ -128,7 +149,7 @@
    :from 'human
 
    ;; Our 'jdoc column is Postgres type jsonb so j-> returns
-   ;; Postgres jsonb, thus we apply jsonb ops to it...
+   ;; Postgres jsonb, thus we apply jsonb functions to it...
    :where (:and (:or (:@> 'jdoc filter))                     ;; Postgres json 'containment' op
                 (:= (:jsonb-array-length (j-> "friends")) 1) ;; Postgres jsonb function
                 (:~ (j->> "email") email-regex))))           ;; Postgres regex function
@@ -139,27 +160,41 @@
    :from 'human
    :inner-join 'gift
    :on (:= (j-> human "key") (j-> gift "human-key"))
+   :where (:= (j-> gift "quantity") (to-json 1))
 
-   ;; We could also write the below :where clause as
+   ;; We could also write the above :where clause as
    ;; :where (:= (:type (j->> gift "quantity") int4) 1)
+   ;; which is what we did with min-balance in the rich-humans$ query
+   ;; above.  j->> asks that gift "quantity" be converted to Postgres
+   ;; type text, which we then cast to Postgres type int4 to compare
+   ;; with 1.
 
-   ;; j->> asks that gift "quantity" be converted to Postgres type
-   ;; text, which we then cast to Postgres type int4 to compare with 1.
-
-   ;; What we actually do below do is convert Postgres type integer 1
-   ;; to Postgres type json which (sadly) we must then explicitly cast
-   ;; to Postgres type jsonb before comparing with key "quantity".
-   ;; Postgres operator -> gives the raw json (or jsonb) of the key
-   ;; while Postgres operator ->> converts it to text.  This library
-   ;; only uses Postgres type jsonb: see the Postgres docs at
+   ;; What we actually do is convert Postgres type integer 1 to
+   ;; Postgres type jsonb (using a little syntactic sugar) before
+   ;; comparing it with key "quantity".  Postgres operator -> gives
+   ;; the raw jsonb of the key while Postgres operator ->>
+   ;; converts it to text.  This library only uses Postgres type
+   ;; jsonb. See also the Postgres docs at
    ;; http://www.postgresql.org/docs/9.4/static/functions-json.html
-   :where (:= (j-> gift "quantity") (:type (:to-json 1) jsonb))))
+   ))
 
-(defun drop ()
-  (drop-model! 'human)
-  (drop-model! 'gift))
+(defun query-test ()
+  (with-pj-conn ()
+    (show (rich-humans$ 3900 "male"))
+    (show (one-friend-humans$ (obj "gender" "female") "^c"))
 
-;; Use the latatude info...
+    (when (zerop (pj:count 'gift))
+      (distribute-gifts))
+    (show (uncharitable-humans$)))
+  (values))
+
 ;; Make connection and set search path immediatley after
-;; Foreign keys?  Promote to foreign key?
-;; Domain consolidation? (enumerate 'human "eyeColor")
+;; Foreign keys?  Promote to foreign key?  Yep, can't do in JSON...
+;; Domain consolidation? (distinct 'human "eyeColor")
+;; Is the nsubst as we walk tree buggy?  Can we not save
+;; up all desired subs and do them all together?
+
+;; We could promote to foreign key:
+;; What key and Postgres type in your child model?  What master table?
+;; We make a column, populate it, add the index
+;; For every insert, we grab that named key and stuff it into the FKey col
