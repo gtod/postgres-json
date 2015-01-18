d2dd787ac2dc4d7b9fade56f7274587ea620f3d9
Gregory Tod <vlad@gtod.net>
2015-01-18 12:52:03 +1100

    Cleanup specials for CLOS models

--- specials.lisp
+++ specials.lisp
@@ -3,42 +3,14 @@
 
 (in-package :postgres-json)
 
-;;;; Model backend specials
-
-;;; These specials are effectively constants.  You _could_ rebind them
-;;; but you would need to do it for every use of every interface
-;;; function.  If you do need different values you would be better off
-;;; just _redefining_ these in some file in your own project.
-
 (defvar *pgj-schema* 'pgj-model
-  "A symbol being the name of the PostgreSQL schema we create to house
-all database backend objects.  If you rebind it said binding must be
-in place for _all_ calls to exported functions of :postgres-json.")
+  "A symbol being the name of the Postgres schema created to house all
+database backend objects.")
 
-;; No reason for user to change this, it sits in a fresh schema we made.
-(defvar *meta-model* 'pgj-meta
-  "A symbol being the name of the model in which we store meta data
-relating to user models.")
-
-;;;; Export specials for client rebinding or redefinition
-
-(defvar *to-json* #'to-json
-  "Function designator for function of one argument to serialize lisp
-objects (submitted to INSERT and UPDATE, for example) to JSON.  Bind
-it at run time for use by the model interface functions.  Or redefine
-it globally for use in your own project.")
-
-(defvar *from-json* #'yason:parse
-  "Function designator for function of one argument to make lisp
-objects from JSON strings retrieved from the DB backend.  Used by GET,
-for example.  Bind it at run time for use by the model interface
-functions.  Or redefine it globally for use in your own project.")
-
-(defvar *stash-key* #'stash-key-destructive
-  "Function designator for function of two arguments: the value of the
-unique primary key to be used for the DB insert and the object to be
-inserted itself.  It should return an object which will be inserted in
-the place of the original.")
+(defvar *pgj-sequence* 'pgj-seq
+  "A symbol being the name of the default Postgres sequence created to
+automatically generate primary keys for JSON documents inserted into a
+backend model.")
 
 ;; I think it sort of makes sense not to sleep at all for the first
 ;; retry, but then to back off pretty fast.  But I am no expert...
@@ -51,8 +23,16 @@ between 0 and 2000.  However, if 0 sleep is specified, we do not sleep
 at all.  If set to NIL no condition handling is performed hence the
 client will always see any such serialization failures.")
 
-(defparameter *postmodern-connection* nil
-  "Set this to a list congruent with the parameters expected by
-POSTMODERN:CONNECT-TOPLEVEL, for use by the testing and example
-code.")
+;; We are not locking write access to this hash so it is not thread
+;; safe.  See postgres-json-parallel.asd and parallel.lisp for one
+;; solution...
+(defvar *query-functions* (make-hash-table :test #'equal)
+  "Hash of schema/model/query-name => query function.")
 
+(defvar *from-json* #'from-json
+  "A function designator for a function of one argument which returns
+the result of parsing the JSON string being its input.")
+
+(defvar *to-json* #'to-json
+  "A function designator for a function of one argument which
+serializes a lisp object to a JSON string.")
